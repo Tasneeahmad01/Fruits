a86f60d234a2d19cd7087d18ba3caf59e87cc3c2
Your Name <you@example.com>
2024-07-06 12:38:04 +0530

    add

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,3 @@
-// import React from 'react'
-
-// function Home() {
-//   return (
-//     <div>
-// <h1>This is Home Page</h1>
-      
-//     </div>
-//   )
-// }
-
-// export default Home
-
-
-
 
 import React from 'react'
 function Home() {
